08d9d4ca77fb0e37a24b138aa5e361b08e94e997
Nelson Andres Giraldo Montoya <nelson.giraldo@globant.com>
2021-04-06 20:07:42 -0500

    Create sum and subtract methods

--- src/operations/Basic.java
+++ src/operations/Basic.java
@@ -3,6 +3,16 @@ package operations;
 public class Basic {
 
 	public static void main(String[] args) {
+		sum(1,4);
+		subtract(2, 1);
+	}
+	
+	public static void sum(double a, double b) {
+		System.out.println(a+b);
+	}
+	
+	public static void subtract(double a, double b) {
+		System.out.println(a-b);
 	}
 	
 
