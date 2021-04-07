8211038dda141360b28e0f16630580694fd32de2
Nelson Andres Giraldo Montoya <nelson.giraldo@globant.com>
2021-04-06 20:10:12 -0500

    create division and multiplication methods

--- src/operations/Basic.java
+++ src/operations/Basic.java
@@ -5,6 +5,8 @@ public class Basic {
 	public static void main(String[] args) {
 		sum(1,4);
 		subtract(2, 1);
+		division(8,4);
+		multiplication(2, 4);
 	}
 	
 	public static void sum(double a, double b) {
@@ -13,6 +15,14 @@ public class Basic {
 	
 	public static void subtract(double a, double b) {
 		System.out.println(a-b);
+	}
+	
+	public static void division(double a, double b) {
+		System.out.println(a/b);
+	}
+	
+	public static void multiplication(double a, double b) {
+		System.out.println(a*b);
 	}
 	
 
